916165e8782709babe9076a9da3294cc30a6eebb
shawnping <shawnping@outlook.com>
2019-04-24 00:08:53 +0800

    完成构造函数成员函数，但是有奇怪的报错涉及ostream和istream类的参数w

--- struct function test project/Sales_data.h
+++ struct function test project/Sales_data.h
@@ -23,6 +23,21 @@ Sales_data add(const Sales_data&, const Sales_data&);
 std::ostream& print(std::ostream&, const Sales_data&);
 std::istream& read(std::istream&, Sales_data&);
 
+std::istream &read(std::istream &is, Sales_data &item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >>price;
+	item.revenues = price * item.units_sold;
+	return is;
+
+}
+
+std::ostream &print(std::ostream &os,const Sales_data &item)
+{
+	os << item.isbn() << " " << item.units_sold << " "
+		<< item.revenues << " " << item.avg_price();
+	return os;
+}
 Sales_data& Sales_data::combine(const Sales_data &rhs)
 {
 	units_sold += rhs.units_sold;
